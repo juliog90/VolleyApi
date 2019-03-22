6761ed175d21a0e8e0bedc0742ed60deabfd32cc
Javier Valencia <weekend58@yahoo.com>
2019-03-22 00:18:39 -0700

    Llamando a Listview desde el Main

--- app/src/main/java/com/juliodev/volleystuff/MainActivity.java
+++ app/src/main/java/com/juliodev/volleystuff/MainActivity.java
@@ -2,12 +2,17 @@ package com.juliodev.volleystuff;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listviewid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listviewid = (ListView)findViewById(R.id.idListView);
     }
 }
